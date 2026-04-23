a41189dfdbc92a89e399a5d7d564e661661d6ea0
terminator <mimiii__@outlook.com>
2026-04-23 18:24:13 +0200

    going un-dendthretic

--- modules/hosts/my-machine/default.nix
+++
@@ -1,9 +0,0 @@
-{ self, inputs, ... }:
-{
-  flake.nixosConfigurations.myMachine = inputs.nixpkgs.lib.nixosSystem {
-    modules = [
-      self.nixosModules.myMachineConfiguration
-      self.nixosModules.myMachineHardware
-    ];
-  };
-}
